4401f32ef6d84f5c8be17b16a142fbf9eaae6f1b
Darshit98 <114256527+Darshit98@users.noreply.github.com>
2023-03-28 20:39:40 -0400

    updated TourCard.jsx

--- frontend/src/shared/TourCard.jsx
+++ frontend/src/shared/TourCard.jsx
@@ -6,24 +6,37 @@ import './tour-card.css';
 
 const TourCard = ({ tour }) => {
 
-  const { id, title, photo, price, featured, avgRating, reviews } = tour
+  const { id, title, photo, price, featured, reviews } = tour
+
+  const totalRating = reviews?.reduce((acc,item)=>acc + item.rating, 0)
+  const avgRating = 
+    totalRating === 0 
+      ? "" 
+      : totalRating === 1
+      ? totalRating 
+      : totalRating / reviews?.length;
 
   return <div className='tour_card'>
     <Card>
       <div className='tour_img'>
         <img src={photo} alt='tour-img' />
-        <span>Featured</span>
+        {featured && <span>Featured</span>}
       </div>
-    </Card>
 
-    <CardBody>
+
+      <CardBody>
 
       <div className="card _top d-flex align-items-center justify-content-between">
         <span className="tour location d-flex align-items-center gap-1">
           <i class="ri-map-pin-line"></i> {city}
         </span>
         <span className="tour _rating d-flex align-items-center gap-1">
-          <i class="ri-star-fill"></i> {avgRating}{" "}
+          <i class="ri-star-fill"></i> {avgRating === 0 ? null : avgRating}
+          {totalRating === 0 ? (
+            "Not rated"
+          ) : (
+            <span> ({reviews.length}) </ span>  
+          )}
           <span> ({reviews.length}) </ span>
         </span>
       </div>
@@ -40,6 +53,9 @@ const TourCard = ({ tour }) => {
 
       </div>
     </CardBody>
+    </Card>
+
+    
   </div>
 };
 
