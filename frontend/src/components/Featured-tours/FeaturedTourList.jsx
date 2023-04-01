47871893e5d1d3f89810b973f562b0a9730673a5
harshjjain <114498782+harshjjain@users.noreply.github.com>
2023-04-01 16:19:29 -0400

    Add files via upload

--- frontend/src/components/Featured-tours/FeaturedTourList.jsx
+++ frontend/src/components/Featured-tours/FeaturedTourList.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import TourCard from '../../shared/TourCard'
 import tourData from '../../assets/data/tours' 
 import { Col } from 'reactstrap'
+//import tours from '../../assets/data/tours'
 
 
 const FeaturedTourList = () => {
-  return <>
-    
+  return(
+  <>
+    {tourData?.map(tour=>(
+      <Col lg='3' className='mb-4' key={tour.id}>
+        <TourCard tour={tour} />
+      </Col>
+    ))}    
   </>
-}
+  );
+};
 
 export default FeaturedTourList
