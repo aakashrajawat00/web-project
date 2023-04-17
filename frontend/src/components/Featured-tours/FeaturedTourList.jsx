5c355c449013e518788fc13ca899eb76b551591c
mehta-vatsal <114501220+mehta-vatsal@users.noreply.github.com>
2023-04-17 15:07:32 -0400

    added tour search page

--- frontend/src/components/Featured-tours/FeaturedTourList.jsx
+++ frontend/src/components/Featured-tours/FeaturedTourList.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import TourCard from '../../shared/TourCard'
-import tourData from '../../assets/data/tours' 
+
 import { Col } from 'reactstrap'
 //import tours from '../../assets/data/tours'
 import useFetch from './../../hooks/useFetch'
@@ -9,13 +9,19 @@ import {BASE_URL} from './../../utils/config'
 
 const FeaturedTourList = () => {
 
-  const {data: featuredTours} = useFetch(`${BASE_URL}/search/getFeaturedTours`);
+  const {data: featuredTours, loading, error} = useFetch(`${BASE_URL}/tours/search/getFeaturedTours`);
 
   console.log(featuredTours);
 
   return(
   <>
-    {featuredTours?.map(tour=>(
+      {
+        loading && <h4>Loading................</h4>
+      }
+       {
+        error && <h4>{error}</h4>
+      }
+    {!loading && !error && featuredTours?.map(tour=>(
       <Col lg='3' className='mb-4' key={tour._id}>
         <TourCard tour={tour} />
       </Col>
