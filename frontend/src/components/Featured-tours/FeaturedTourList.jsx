dfe24aa03feabe375d097f9c25665b50010a3f7c
aakashrajawat00 <rajawat.a@northeastern.edu>
2023-04-17 13:10:49 -0400

    Revert "added hooks folder and useFetch.js file inside"
    This reverts commit 33e9382345e4227c5b6681ef9f4622aa9c6293a9.

--- frontend/src/components/Featured-tours/FeaturedTourList.jsx
+++ frontend/src/components/Featured-tours/FeaturedTourList.jsx
@@ -3,20 +3,13 @@ import TourCard from '../../shared/TourCard'
 import tourData from '../../assets/data/tours' 
 import { Col } from 'reactstrap'
 //import tours from '../../assets/data/tours'
-import useFetch from './../../hooks/useFetch'
-import {BASE_URL} from './../../utils/config'
 
 
 const FeaturedTourList = () => {
-
-  const {data: featuredTours} = useFetch(`${BASE_URL}/search/getFeaturedTours`);
-
-  console.log(featuredTours);
-
   return(
   <>
-    {featuredTours?.map(tour=>(
-      <Col lg='3' className='mb-4' key={tour._id}>
+    {tourData?.map(tour=>(
+      <Col lg='3' className='mb-4' key={tour.id}>
         <TourCard tour={tour} />
       </Col>
     ))}    
