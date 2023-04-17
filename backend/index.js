8ecd14417c36195c22902137f2df0f404786b782
harshjjain <114498782+harshjjain@users.noreply.github.com>
2023-04-16 21:11:56 -0400

    Authorization for User and Admin

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,10 @@ import authRoute from './routes/auth.js'
 dotenv.config()
 const app = express()
 const port = process.env.PORT || 8000
+const corsOptions = {
+    origin:true,
+    credentials:true
+}
 
 //database connection s
 mongoose.set("strictQuery", false);
@@ -30,11 +34,11 @@ const connect = async () => {
 
 //middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(cookieParser());
-app.use('/auth', authRoute);
-app.use('/tours', tourRoute);
-app.use('/users', userRoute);
+app.use('/api/v1/auth', authRoute);
+app.use('/api/v1/tours', tourRoute);
+app.use('/api/v1/users', userRoute);
 
 app.listen(port, () => {
     connect();
