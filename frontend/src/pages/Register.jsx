fbde288d0611e0c44bd88e10a4782d274153543e
aakashrajawat00 <114462883+aakashrajawat00@users.noreply.github.com>
2023-04-18 19:36:23 -0400

    Register and Login success

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,46 +1,70 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import {Container, Row, Col, Form, FormGroup, Button} from 'reactstrap';
-import {Link } from 'react-router-dom';
-//import '../styles/login.css'
+import {Link, useNavigate } from 'react-router-dom';
+import '../styles/login.css';
 
 import registerImg from '../assets/images/register.png'
 import userIcon from '../assets/images/user.png'
+//import { AuthContext } from '../context/AuthContext';
+import { AuthContext } from '../context/AuthContext';
 
+import { BASE_URL } from '../utils/config';
 
 const Register = () => {
-  const [credentials, setcredentials] = useState({
+  const [credentials, setCredentials] = useState({
     userName:undefined,
     email:undefined,
     password:undefined,
 
 });
 
+  const {dispatch} = useContext(AuthContext)
+  const navigate = useNavigate()
+
   const handleChange = e => {
-    setcredentials(prev=>({...prev, [e.target.id]:e.target.value}))
+    setCredentials(prev=>({...prev, [e.target.id]:e.target.value}))
 
 }; 
 
-const handleClick = e =>{
+const handleClick = async e =>{
   e.preventDefault();
+
+  try {
+    const res = await fetch(`${BASE_URL}/auth/register`,{
+      method:'post',
+      headers:{
+        'content-type':'application/json'
+      },
+      body: JSON.stringify(credentials)
+    })
+    const result = await res.json()
+
+    if(!res.ok) alert(result.message)
+    dispatch({type:'REGISTER_SUCCESS'})
+    navigate('/login')
+  } catch (err) {
+    alert(err.message);
+    
+  }
 };
 
   return <section>
-    <Container >
-      <Row >
+    <Container>
+      <Row>
         <Col lg='8' className='m-auto'>
           <div className='login__container d-flex justify-content-between'>
             <div className='login__img'>
-              <img src={registerImg} ></img>
+              <img src={registerImg} alt="" ></img>
 
             </div>
             <div className="login__form">
               <div className="user">
-                <img src={userIcon}/>
+                <img src={userIcon} alt=""/>
               </div>
               <h2>Register</h2>
               <Form onSubmit={handleClick}>
               <FormGroup>
-                  <input type="text" placeholder='Username' required id="Username" onChange={handleChange}/>
+                  <input type="text" placeholder='Username' required id="username" onChange={handleChange}/>
                 </FormGroup>
                 <FormGroup>
                   <input type="email" placeholder='Email' required id="email" onChange={handleChange}/>
@@ -50,7 +74,7 @@ const handleClick = e =>{
                 </FormGroup>
                 <Button className='btn secondary__btn auth__btn' type='submit'>Create Account</Button>
               </Form>
-              <p>Already have an account?<Link to='/Login'>Login</Link></p>
+              <p>Already have an account?<Link to='/login'>Login</Link></p>
             </div>
           </div>
         </Col>
@@ -59,4 +83,4 @@ const handleClick = e =>{
   </section>
 }
 
-export default Register
+export default Register;
