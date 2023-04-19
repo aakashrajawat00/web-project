a10d481354e69f29c2905548da55610e58649829
Darshit98 <114256527+Darshit98@users.noreply.github.com>
2023-04-18 20:57:01 -0400

    Add files via upload

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import {Container, Row, Button} from 'reactstrap'
-import {NavLink, Link} from 'react-router-dom'
+import React, { useRef, useEffect, useContext } from 'react'
+import { Container, Row, Button } from 'reactstrap'
+import { NavLink, Link, useNavigate } from 'react-router-dom'
 import logo from '../../assets/images/logo.png'
 import "./Header.css"
+import { AuthContext } from '../../context/AuthContext'
 
 const nav_links = [
   {
@@ -17,16 +18,42 @@ const nav_links = [
     path: './tours',
     display: 'Tours'
   }
-  
+
 ]
 const Header = () => {
-  return <header className="header">
+
+  const headerRef = useRef(null);
+  const navigate = useNavigate()
+  const {user, dispatch} = useContext(AuthContext)
+
+  const logout = ()=>{
+    dispatch({type:'LOGOUT'})
+    navigate('/')
+  }
+
+  const stickyHeaderFunc = () => {
+    window.addEventListener('scroll', () => {
+      if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        headerRef.current.classList.add('sticky__header')
+      } else {
+        headerRef.current.classList.remove('sticky__header')
+      }
+    })
+  }
+
+  useEffect(() => {
+    stickyHeaderFunc()
+
+    return window.removeEventListener('scroll', stickyHeaderFunc)
+  })
+
+  return <header className="header" ref={headerRef}>
     <Container >
       <Row>
         <div className="nav_wrapper d-flex align-items-center justify-content-between">
           {/*=================Logo===============*/}
           <div className="logo">
-            <img src={logo} alt=''/>
+            <img src={logo} alt='' />
           </div>
           {/*=================Logo end ========== */}
 
@@ -34,9 +61,9 @@ const Header = () => {
           <div className="navigation">
             <ul className="menu d-flex align-items-center gap-5">
               {
-                nav_links.map((item,index)=>(
+                nav_links.map((item, index) => (
                   <li className='nav__item' key={index}>
-                    <NavLink to={item.path} className={navClass=> navClass.isActive? 'active__link':''}>{item.display}</NavLink>
+                    <NavLink to={item.path} className={navClass => navClass.isActive ? 'active__link' : ''}>{item.display}</NavLink>
 
                   </li>
                 ))
@@ -47,8 +74,22 @@ const Header = () => {
           {/*=================Menu end ========== */}
           <div className="nav_right d-flex align-items-center  gap-4">
             <div className="nav_btns d-flex align-items-center  gap-4">
-              <Button className='btn secondary__btn'> <Link to='/login'>Login</Link></Button>
-              <Button className='btn primary__btn'> <Link to='/Register'>Register</Link></Button>
+
+              {
+                user? (
+                <>
+                <h5 className="mb-0">{user.username}</h5>
+                <Button className="btn btn-dark" onClick={logout}>Logout</Button>
+                </>
+                ) : (
+                <>
+
+                <Button className='btn secondary__btn'> <Link to='/login'>Login</Link></Button>
+                <Button className='btn primary__btn'> <Link to='/Register'>Register</Link></Button>
+
+                </>
+              )}
+              
             </div>
 
             <span className="mobile__menu">
